a7e9f1de2e4e1a4f4b41c8593ea831a16e0eccb5
GrapeBird <shvann@hiof.no>
2024-11-14 20:27:51 +0100

    .

--- oppgave_1/frontend/src/app/courses/[courseSlug]/lessons/page.tsx
+++ oppgave_1/frontend/src/app/courses/[courseSlug]/lessons/page.tsx
@@ -1,14 +1,14 @@
-// src/app/courses/[courseSlug]/lessons/page.tsx
-import React from "react";
-import Lesson from "@/features/lesson/components/Lesson";
-import { useParams } from "next/navigation";
+// // src/app/courses/[courseSlug]/lessons/page.tsx
+// import React from "react";
+// import Lesson from "@/features/lesson/components/Lesson";
+// import { useParams } from "next/navigation";
 
-const LessonsListPage: React.FC = () => {
-  const { courseSlug, lessonSlug } = useParams();
+// const LessonsListPage: React.FC = () => {
+//   const { courseSlug, lessonSlug } = useParams();
 
-  return (
-    <main>
-      <Lesson courseSlug={courseSlug} lessonSlug={lessonSlug} />
-    </main>
-  );
-};
+//   return (
+//     <main>
+//       <Lesson courseSlug={courseSlug} lessonSlug={lessonSlug} />
+//     </main>
+//   );
+// };
