c042e14aac5b4cdd4467c2834e77bb856490893a
GrapeBird <shvann@hiof.no>
2024-11-14 21:06:33 +0100

    .

--- oppgave_1/frontend/src/app/page.tsx
+++ oppgave_1/frontend/src/app/page.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-//import Layout from "../components/Layout"; // Adjust path if needed
 import SignUp from "../components/SignUp"; // Adjust path if needed
 export default function Home() {
   return (
     <main className="h-full">
-      {/* <p>Siden er tom</p> 
-      Eksisterer ikke på wireframes men er i All.js. 
-      */}
-
-      {/* SignUp component will be displayed as the main content */}
       <SignUp />
     </main>
   );
