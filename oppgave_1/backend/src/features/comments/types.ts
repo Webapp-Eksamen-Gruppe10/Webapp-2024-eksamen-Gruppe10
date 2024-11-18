e5f6c3850c2251d277dfa98dca482834fafe61ef
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-11-18 17:27:28 +0100

    prøver å fikse type-problemet med text i lessons i gets.

--- oppgave_1/backend/src/features/comments/types.ts
+++ oppgave_1/backend/src/features/comments/types.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 
 // todo: fix zod validation.
+/*  {
+    id: '1',
+    createdBy: { id: '2', name: 'Sara Olsen' },
+    comment:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    lesson: { slug: 'variabler' },
+  },*/
+  
 export const commentSchema = z.object({
     id: z.string(),
     createdBy: z.object({
@@ -11,4 +19,11 @@ export const commentSchema = z.object({
     lesson: z.object({
         slug: z.string(),
     }),
+});
+
+export const commentDbSchema = z.object({
+    id: z.string(),
+    createdBy: z.string(),
+    comment: z.string(),
+    lesson: z.string(),
 });
