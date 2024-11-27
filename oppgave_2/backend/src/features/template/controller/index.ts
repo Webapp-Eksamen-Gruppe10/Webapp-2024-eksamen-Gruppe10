b465bd040f32000db84036f39def9e0d27292397
Mahamoud Ibrahim <mahamod.ibrahim@gmail.com>
2024-11-28 00:22:57 +0100

    Lager controller for template for å fullføre lagdeling for denne feature

---
+++ oppgave_2/backend/src/features/template/controller/index.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+import { TemplateService } from "../service";
+import { cors } from "hono/cors";
+import { errorResponse } from "@/lib/error";
+
+export const createTemplateController = (templateServiceDb: TemplateService) => {
+    const app = new Hono();
+
+    app.use("/*", cors())
+
+    app.get("/", async (c) => {
+        const result = await templateServiceDb.getAllTemplates()
+
+        if(!result.success)
+            return errorResponse(c, result.error.code, result.error.message)
+        return c.json(result)
+    });
+
+    app.get("/:id", async(c) => {
+        const id = c.req.param("id")
+        const result = await templateServiceDb.getOneTemplate(id)
+
+        if(!result.success)
+            return errorResponse(c, result.error.code, result.error.message)
+        return c.json(result)
+    })
+}
