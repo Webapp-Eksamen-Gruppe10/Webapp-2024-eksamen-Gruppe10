dc8a5da60fd4ca0a2d6e7190b933564b494189f0
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-11-28 10:13:15 +0100

    jobber med å implementere adminevents.

---
+++ oppgave_2/frontend/src/features/event/components/AdminEvents.tsx
@@ -0,0 +1,11 @@
+
+import { Event } from "@/features/event/lib/schema";
+
+type AdminEventsProps = {
+    events: Event[]
+
+}
+
+export default function AdminEvents({events}: AdminEventsProps) {
+
+}
