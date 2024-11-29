18db24f95b1d6feeefdd37e1be25b86eb856662d
Mahamoud Ibrahim <mahamod.ibrahim@gmail.com>
2024-11-29 23:59:25 +0100

    Fullførte controller til registration

--- oppgave_2/backend/src/features/registration/controller/index.ts
+++ oppgave_2/backend/src/features/registration/controller/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { registrationService, RegistrationService } from "../service";
 import { cors } from "hono/cors";
 import { errorResponse } from "../../../lib/error";
+import { Registration } from "../types";
+import { Data } from "../../../types";
 
 export const createRegistrationController = (
     registrationServiceDb: RegistrationService
@@ -21,6 +23,34 @@ export const createRegistrationController = (
       app.get("/:id", async (c) => {
         const id = c.req.param("id");
         const result = await registrationServiceDb.getOneRegistration(id);
+    
+        if (!result.success)
+          return errorResponse(c, result.error.code, result.error.message);
+        return c.json(result);
+      });
+
+      app.post("/", async (c) => {
+        const data = await c.req.json();
+        const result = await registrationServiceDb.createRegistration(data);
+    
+        if (!result.success)
+          return errorResponse(c, result.error.code, result.error.message);
+        return c.json<Data<Registration>>(result, { status: 201 });
+      });
+    
+      app.patch("/:id", async (c) => {
+        const id = c.req.param("id");
+        const data = await c.req.json();
+        const result = await registrationServiceDb.updateRegistration(data, id);
+    
+        if (!result.success)
+          return errorResponse(c, result.error.code, result.error.message);
+        return c.json(result);
+      });
+    
+      app.delete("/:id", async (c) => {
+        const id = c.req.param("id");
+        const result = await registrationServiceDb.deleteRegistration(id);
     
         if (!result.success)
           return errorResponse(c, result.error.code, result.error.message);
