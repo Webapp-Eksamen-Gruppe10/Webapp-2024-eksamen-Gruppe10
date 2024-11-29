27217d768ff42c77e909c120d18785ba0e390402
GrapeBird <shvann@hiof.no>
2024-11-29 12:32:23 +0100

    event crud functionality settup

--- oppgave_2/backend/src/app.ts
+++ oppgave_2/backend/src/app.ts
@@ -2,8 +2,6 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { endpoint } from "./config/url";
 import { eventController } from "./features/events/controller";
-import { templateController } from "./features/template/controller";
-// import { templateController } from "./features/template/controller";
 
 const app = new Hono();
 
@@ -11,7 +9,6 @@ app.use("/*", cors());
 
 // lagde ny events ettersom denne skal ikke ha med baseurl, men kun /api/v1/events
 app.route(endpoint.event, eventController);
-app.route(endpoint.template, templateController);
 
 app.onError((err, c) => {
   console.error(err);
