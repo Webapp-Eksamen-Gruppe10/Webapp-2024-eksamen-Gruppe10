c33ee247e2faf2992e20b7fa940d331c3f6a08a1
Mareamsefan <sefanmaream@gmail.com>
2024-12-01 01:44:35 +0100

    rettet opp stylingen av dato-lable i AdminCreateEvent.

--- oppgave_2/frontend/src/features/event/components/AdminCreateEvent.tsx
+++ oppgave_2/frontend/src/features/event/components/AdminCreateEvent.tsx
@@ -109,9 +109,12 @@ export default function AdminCreateEventForm({ selectedTemplateId, selectedTempl
           </div>
 
           <div className="space-y-2">
-          <label>
+          <label htmlFor="dato" className="block text-sm font-medium text-gray-700">  
+            Dato
+            </label>
               {selectedTemplateId.length > 0 ? (
                 <DatePicker
+                  id="dato"
                   required
                   selected={date}
                   onChange={(newDate) => setDate(newDate)}
@@ -130,6 +133,7 @@ export default function AdminCreateEventForm({ selectedTemplateId, selectedTempl
                 />
               ) : (
                 <DatePicker
+                  id="dato"
                   required
                   selected={date}
                   onChange={(newDate) => setDate(newDate)}
@@ -141,7 +145,7 @@ export default function AdminCreateEventForm({ selectedTemplateId, selectedTempl
                   className="block w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
                 />
               )}
-          </label>
+       
           </div>
 
           <div className="space-y-2">
